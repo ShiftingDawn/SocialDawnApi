9465fb60d7329db146e7073eb9513fd681f0abcc
ShiftingDawn <191372323+ShiftingDawn@users.noreply.github.com>
2024-12-26 13:12:51 +0100

    Add TypeORM

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,22 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { Module } from "@nestjs/common";
+import { AppController } from "./app.controller";
+import { AppService } from "./app.service";
+import { TypeOrmModule } from "@nestjs/typeorm";
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+	imports: [
+		TypeOrmModule.forRoot({
+			type: "postgres",
+			host: "localhost",
+			port: 5432,
+			username: "socialdawn",
+			password: "socialdawn",
+			database: "socialdawn",
+			entities: [],
+			synchronize: true,
+		}),
+	],
+	controllers: [AppController],
+	providers: [AppService],
 })
 export class AppModule {}
