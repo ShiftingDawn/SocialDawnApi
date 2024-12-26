9b4fdb0a97933aa90c33c57bc84b52212ccbdc7c
ShiftingDawn <191372323+ShiftingDawn@users.noreply.github.com>
2024-12-26 13:12:54 +0100

    Added more validators to register dto

--- src/user/register.dto.ts
+++ src/user/register.dto.ts
@@ -1,12 +1,17 @@
-import { IsEmail, IsNotEmpty, IsStrongPassword } from "class-validator";
+import { IsEmail, IsNotEmpty, IsStrongPassword, Length, Matches } from "class-validator";
 
 export class RegisterDTO {
-	@IsNotEmpty()
+	@IsNotEmpty({ message: "invalid_username" })
+	@Length(3, 32, { message: "invalid_username" })
+	@Matches(/^[a-zA-Z0-9][a-zA-Z0-9-_]+[a-zA-Z0-9]$/)
 	username: string;
 
-	@IsEmail()
+	@IsEmail(undefined, { message: "invalid_email" })
 	email: string;
 
-	@IsStrongPassword({ minLength: 8, minLowercase: 1, minUppercase: 1, minNumbers: 1, minSymbols: 0 })
+	@IsStrongPassword(
+		{ minLength: 8, minLowercase: 1, minUppercase: 1, minNumbers: 1, minSymbols: 0 },
+		{ message: "password_too_weak" },
+	)
 	password: string;
 }
